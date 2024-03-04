6f1087effcdd5b770a7f717db1835934a681495a
FlxAV <felix.a.vidal@gmail.com>
2024-03-05 11:18:55 +1300

    v_03

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -7,6 +7,14 @@ function draw_one_frame(cur_frac) {
 
     let b1_y = height/2; //0.55 * height;
     let b1_size = 50;
+    let numCols = 10;
+    let numRows = 10;
+    let cellWidth = width / numCols;
+    let cellHeight = height / numRows;
+    let startX = 0;
+    let startY = height / 2;
+    let direction = 1; // 1 for moving right, -1 for moving left
+
 
  
 	//console.log(cur_frac);
@@ -57,3 +65,53 @@ function draw_one_frame(cur_frac) {
         trail.shift(); // Remove the oldest position if the trail exceeds 100 positions
     }
 }
+
+
+// function draw_one_frame(cur_frac) {
+//     // Background
+//     fill(255, 255, 255);
+//     rect(0, 0, width, height);
+
+//     let b1_size = 50;
+//     let numCols = 10;
+//     let numRows = 10;
+//     let cellWidth = width / numCols;
+//     let cellHeight = height / numRows;
+//     let startX = 0;
+//     let startY = height / 2;
+//     let direction = 1; // 1 for moving right, -1 for moving left
+
+//     // Draw the line
+//     for (let col = 0; col < numCols; col++) {
+//         for (let row = 0; row < numRows; row++) {
+//             let x = startX + col * cellWidth;
+//             let y = startY + (row - numRows / 2) * cellHeight;
+
+//             if (col / numCols <= cur_frac) {
+//                 fill(255, 0, 0);
+//                 noStroke();
+//                 rect(x, y, b1_size, b1_size);
+//             }
+//         }
+//     }
+
+//     // Update start position based on the fraction
+//     if (cur_frac >= 0.5) {
+//         startX += direction * cellWidth;
+//         startY -= direction * cellHeight / 2;
+//         direction = -1; // Change direction to go upwards
+//     }
+
+//     // Remove the line
+//     if (cur_frac === 1) {
+//         fill(255, 255, 255);
+//         for (let col = 0; col < numCols; col++) {
+//             for (let row = 0; row < numRows; row++) {
+//                 let x = startX + col * cellWidth;
+//                 let y = startY + (row - numRows / 2) * cellHeight;
+
+//                 rect(x, y, b1_size, b1_size);
+//             }
+//         }
+//     }
+// }
